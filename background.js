c300eaa895e7b4bd3b95f710d567de93f4756aca
Yujin <46616793+bellayujin@users.noreply.github.com>
2021-01-17 22:43:04 +0900

    fix: images path

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 5;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `/images/${imgNumber + 1}.jpg`;
+    image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImg");
     body.appendChild(image);
 };
